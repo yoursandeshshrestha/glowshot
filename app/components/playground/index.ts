25625cbf22ff31b8a897b07e19397be64d455a20
yoursandeshshrestha <yoursandeshshrestha@gmail.com>
2025-11-04 23:23:17 +0530

    feat(playground): add component exports

---
+++ app/components/playground/index.ts
@@ -0,0 +1,8 @@
+export { Sidebar } from "./Sidebar";
+export { ImageControls } from "./ImageControls";
+export { Canvas } from "./Canvas";
+export { CanvasWithRulers } from "./CanvasWithRulers";
+export { InlineCrop } from "./InlineCrop";
+export { InlineScale } from "./InlineScale";
+export { DownloadMenu } from "./DownloadMenu";
+export { BackgroundDrawer } from "./BackgroundDrawer";
